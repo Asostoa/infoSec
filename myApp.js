985d21233bd410954ae319d4061524c1e3fa2f31
Alex <alexanderdesostoa@gmail.com>
2024-10-13 13:30:05 -0400

    initial document

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(` 🛸 usefull programer info security  ${PORT}`);
+});
 
 
 
